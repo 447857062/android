37bdd4a1f77f7fc8133c2ee0c24120e4f73c5efc
kelijun <447857062@qq.com>
2018-01-09 18:11:46 +0800

    提交

--- SmartWirelessRelay/mqtt-sdk/src/main/java/com/deplink/sdk/android/sdk/homegenius/DeviceAddBody.java
+++ SmartWirelessRelay/mqtt-sdk/src/main/java/com/deplink/sdk/android/sdk/homegenius/DeviceAddBody.java
@@ -3,13 +3,15 @@
   */
 package com.deplink.sdk.android.sdk.homegenius;
 
+import java.io.Serializable;
+
 /**
  * Auto-generated: 2016-07-06 12:3:50
  *
  * @author aTool.org (i@aTool.org)
  * @website http://www.atool.org/json2javabean.php
  */
-public class DeviceAddBody {
+public class DeviceAddBody implements Serializable{
     private String mac;
     private String sn;
     private String device_type;
